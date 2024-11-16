4b65dfbc16ae5ecceea889ee42e13a494733938f
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-11-16 05:24:23 +0100

    Add keyboard support

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,33 @@
 const accordionQuestions = document.querySelectorAll(".q");
 
 accordionQuestions.forEach((question) => {
-  question.addEventListener("click", (e) => {
-    const answer = question.nextElementSibling;
-    const currentIcon = question.querySelector(".toggle-icon");
-
-    accordionQuestions.forEach((question) => {
-      if (question.nextElementSibling !== answer) {
-        question.nextElementSibling.classList.add("hidden");
-        question.querySelector(".toggle-icon").src =
-          "../assets/images/icon-plus.svg";
-      }
-    });
-
-    if (answer.classList.contains("hidden")) {
-      answer.classList.remove("hidden");
-      currentIcon.src = "../assets/images/icon-minus.svg";
-    } else {
-      answer.classList.add("hidden");
-      currentIcon.src = "../assets/images/icon-plus.svg";
+  question.addEventListener("click", () => {
+    toggleAccordion(question);
+  });
+  question.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" || e.keyCode === 32) {
+      toggleAccordion(question);
     }
   });
 });
+
+function toggleAccordion(question) {
+  const answer = question.nextElementSibling;
+  const currentIcon = question.querySelector(".toggle-icon");
+
+  accordionQuestions.forEach((question) => {
+    if (question.nextElementSibling !== answer) {
+      question.nextElementSibling.classList.add("hidden");
+      question.querySelector(".toggle-icon").src =
+        "../assets/images/icon-plus.svg";
+    }
+  });
+
+  if (answer.classList.contains("hidden")) {
+    answer.classList.remove("hidden");
+    currentIcon.src = "../assets/images/icon-minus.svg";
+  } else {
+    answer.classList.add("hidden");
+    currentIcon.src = "../assets/images/icon-plus.svg";
+  }
+}
